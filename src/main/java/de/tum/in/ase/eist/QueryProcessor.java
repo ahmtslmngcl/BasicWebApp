55f63505c668f72742ceaf3b49f1afcaf6e91f96
ahmtslmngcl <77407148+ahmtslmngcl@users.noreply.github.com>
2022-07-14 15:10:11 +0200

    largest number

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,15 +6,25 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		//query = query.toLowerCase();
+		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
+        } if (query.contains("name")) {
            return "app-eistea";
-        } else { // TODO extend the programm here
-            return "";
+        } if (query.contains("numbers")) { // TODO extend the programm here
+            if (query.contains("largest")) {
+                String newQuery = query.split(":");
+                char[] letters = newQuery.toCharArray();
+                int result = 0;
+                for (int i = 0; i <= letters.length - 1; i++) {
+                    if (Integer.parseInt(letters[i]) > result) {
+                        result = Integer.parseInt(letters[i]);
+                    }
+                }
+                return String.valueOf(result);
+            }
         }
     }
 }
